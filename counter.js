4041e7917b76fb5a4d1f2d1c7b10cbf52dc7f129
Jorge Villalobos <79444943+JorgeLVilla@users.noreply.github.com>
2023-10-16 15:08:08 -0700

    create vite project

---
+++ counter.js
@@ -0,0 +1,65 @@
+console.log('hello');
+
+class Song {
+  constructor(id, text, done) {
+    this.id = id;
+    this.text = text;
+    this.done = done;
+  }
+}
+
+const songs = [];
+
+function addSong(text) {
+  const newSong = new Song(songs.length + 1, text, false);
+  songs.push(newSong);
+}
+
+function renderSongs() {
+  const songList = document.getElementById("songList");
+  if (!songList) return;
+
+  songList.innerHTML = "";
+  songs.forEach((song) => {
+    const listItem = document.createElement("li");
+    listItem.innerHTML = `
+            <input type="checkbox" ${song.done ? "checked" : ""}>
+            <span>${song.text}</span>
+        `;
+    listItem.querySelector("input")?.addEventListener("change", (event) => {
+      // @ts-ignore
+      song.done = event.target.checked;
+      renderSongs();
+    });
+    songList.appendChild(listItem);
+  });
+}
+
+const addSongButton = document.getElementById("addSong");
+if (addSongButton) {
+  addSongButton.addEventListener("click", () => {
+    const songInput = document.getElementById("songInput");
+    const songText = songInput.value.trim();
+    if (songText !== "") {
+      addSong(songText);
+      songInput.value = "";
+      renderSongs();
+    }
+  });
+}
+
+const songInput = document.getElementById("songInput");
+if (songInput) {
+  songInput.addEventListener("keydown", (event) => {
+    if (event.key === "Enter") {
+      const songText = songInput.value.trim();
+      if (songText !== "") {
+        addSong(songText);
+        songInput.value = "";
+        renderSongs();
+      }
+    }
+  });
+}
+
+renderSongs();
---
+++ main.js
@@ -0,0 +1,30 @@
+// Helper function to load HTML content into the container
+function loadPage(pageName) {
+    const container = document.getElementById("content-container");
+    const url = `src/${pageName}.html`;
+
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            container.innerHTML = data;
+        })
+        .catch(error => {
+            console.error(`Error loading ${pageName}: ${error.message}`);
+        });
+}
+
+// Event listeners to load pages when links are clicked
+const playlistsLink = document.getElementById("playlists-link");
+playlistsLink.addEventListener("click", () => loadPage("playlists"));
+
+const userLink = document.getElementById("profile-link");
+userLink.addEventListener("click", () => loadPage("profile"));
+
+const contactLink = document.getElementById("contact-link");
+contactLink.addEventListener("click", () => loadPage("contact"));
+
+const aboutLink = document.getElementById("about-link");
+aboutLink.addEventListener("click", () => loadPage("about"));
+
+// Initial page load
+loadPage("profile");
