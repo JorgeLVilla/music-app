1bdc4679bb230cf10d6a1ec06a11986aade7bcda
Jorge Villalobos <79444943+JorgeLVilla@users.noreply.github.com>
2023-10-28 23:44:39 -0700

    adds functionality to be able to edit song

--- src/learn-song.js
+++ src/learn-song.js
@@ -1,5 +1,3 @@
-
-// Define a Song class
 class Song {
     constructor(title, artist, key) {
         this.title = title;
@@ -8,22 +6,37 @@ class Song {
     }
 }
 
-// Function to add a song to the to-do list
 function addSongToList(song) {
     const songList = document.getElementById("songList");
     const listItem = document.createElement("li");
-    listItem.textContent = `${song.title} by ${song.artist}, Key: ${song.key}`;
+    listItem.innerHTML = `${song.title} by ${song.artist}, Key: ${song.key} 
+        <button class="editButton">Edit</button>`;
     songList.appendChild(listItem);
+
+    // Add an event listener to the "Edit" button
+    const editButton = listItem.querySelector(".editButton");
+    editButton.addEventListener("click", () => {
+        populateEditForm(song);
+    });
 }
 
-// Event listener for the form submission
+// Function to populate the edit form with song information
+function populateEditForm(song) {
+    const editTitle = document.getElementById("editTitle");
+    const editArtist = document.getElementById("editArtist");
+    const editKey = document.getElementById("editKey");
+
+    editTitle.value = song.title;
+    editArtist.value = song.artist;
+    editKey.value = song.key;
+}
+
 document.getElementById("songForm").addEventListener("submit", function(event) {
     event.preventDefault();
     const title = document.getElementById("title").value;
     const artist = document.getElementById("artist").value;
     const key = document.getElementById("key").value;
 
-    // Create a new Song instance and add it to the list
     const newSong = new Song(title, artist, key);
     addSongToList(newSong);
 
@@ -31,4 +44,28 @@ document.getElementById("songForm").addEventListener("submit", function(event) {
     document.getElementById("title").value = "";
     document.getElementById("artist").value = "";
     document.getElementById("key").value = "";
+});
+
+document.getElementById("editForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    // Get the edited song information from the form
+    const editedTitle = document.getElementById("editTitle").value;
+    const editedArtist = document.getElementById("editArtist").value;
+    const editedKey = document.getElementById("editKey").value;
+
+    // Update the song in the list with the edited information
+    const songList = document.getElementById("songList");
+    const selectedListItem = songList.querySelector(".selected");
+    if (selectedListItem) {
+        const song = new Song(editedTitle, editedArtist, editedKey);
+        const editButton = selectedListItem.querySelector(".editButton");
+        editButton.textContent = `${editedTitle} by ${editedArtist}, Key: ${editedKey}`;
+        selectedListItem.classList.remove("selected");
+    }
+
+    // Clear the edit form inputs
+    document.getElementById("editTitle").value = "";
+    document.getElementById("editArtist").value = "";
+    document.getElementById("editKey").value = "";
 });
