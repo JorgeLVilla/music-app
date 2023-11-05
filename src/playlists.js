92da971c47889e951d7525b32f6c06682894ddc0
Jorge Villalobos <79444943+JorgeLVilla@users.noreply.github.com>
2023-11-04 23:59:15 -0700

    update about

--- src/playlists.js
+++ src/playlists.js
@@ -1,66 +1,64 @@
 document.addEventListener("DOMContentLoaded", () => {
-    class Song {
-        constructor(id, text, done) {
-            this.id = id;
-            this.text = text;
-            this.done = done;
-        }
+  class Song {
+    constructor(id, text, done) {
+      this.id = id;
+      this.text = text;
+      this.done = done;
     }
+  }
 
-    const songs = [];
+  const songs = [];
 
-    function addSong(text) {
-        const newSong = new Song(songs.length + 1, text, false);
-        songs.push(newSong);
-    }
+  function addSong(text) {
+    const newSong = new Song(songs.length + 1, text, false);
+    songs.push(newSong);
+  }
 
-    function renderSongs() {
-        const songList = document.getElementById("songList");
-        if (!songList) return;
+  function renderSongs() {
+    const songList = document.getElementById("songList");
+    if (!songList) return;
 
-        songList.innerHTML = "";
-        songs.forEach((song) => {
-            const listItem = document.createElement("li");
-            listItem.innerHTML = `
+    songList.innerHTML = "";
+    songs.forEach((song) => {
+      const listItem = document.createElement("li");
+      listItem.innerHTML = `
                 <input type="checkbox" ${song.done ? "checked" : ""}>
                 <span>${song.text}</span>
             `;
-            listItem.querySelector("input")?.addEventListener("change", (event) => {
-                song.done = event.target.checked;
-                renderSongs();
-            });
-            songList.appendChild(listItem);
-        });
-    }
+      listItem.querySelector("input")?.addEventListener("change", (event) => {
+        song.done = event.target.checked;
+        renderSongs();
+      });
+      songList.appendChild(listItem);
+    });
+  }
 
-    const addSongButton = document.getElementById("addSong");
-    if (addSongButton) {
-        addSongButton.addEventListener("click", () => {
-            const songInput = document.getElementById("songInput");
-            const songText = songInput.value.trim();
-            if (songText !== "") {
-                addSong(songText);
-                songInput.value = "";
-                renderSongs();
-            }
-        });
-    }
+  const addSongButton = document.getElementById("addSong");
+  if (addSongButton) {
+    addSongButton.addEventListener("click", () => {
+      const songInput = document.getElementById("songInput");
+      const songText = songInput.value.trim();
+      if (songText !== "") {
+        addSong(songText);
+        songInput.value = "";
+        renderSongs();
+      }
+    });
+  }
 
-    const songInput = document.getElementById("songInput");
-    if (songInput) {
-        songInput.addEventListener("keydown", (event) => {
-            if (event.key === "Enter") {
-                const songText = songInput.value.trim();
-                if (songText !== "") {
-                    addSong(songText);
-                    songInput.value = "";
-                    renderSongs();
-                }
-            }
-        });
-    }
+  const songInput = document.getElementById("songInput");
+  if (songInput) {
+    songInput.addEventListener("keydown", (event) => {
+      if (event.key === "Enter") {
+        const songText = songInput.value.trim();
+        if (songText !== "") {
+          addSong(songText);
+          songInput.value = "";
+          renderSongs();
+        }
+      }
+    });
+  }
 
-    renderSongs();
+  renderSongs();
 });
-
-
