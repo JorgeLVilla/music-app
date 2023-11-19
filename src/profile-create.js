988fbdda22c476e760e297fecc924f0a76d755e3
Jorge Villalobos <79444943+JorgeLVilla@users.noreply.github.com>
2023-11-18 23:52:53 -0800

    tries new ways to edit and save a profile in app

---
+++ src/profile-create.js
@@ -0,0 +1,62 @@
+function saveProfile() {
+  var firstName = prompt("Enter your first name:");
+  var lastName = prompt("Enter your last name:");
+  var username = prompt("Enter your username:");
+  var email = prompt("Enter your email address:");
+
+  var profile = {
+    FirstName: firstName,
+    LastName: lastName,
+    Username: username,
+    Email: email,
+  };
+
+  var profileString = JSON.stringify(profile);
+  localStorage.setItem("profile", profileString);
+
+  console.log("Profile information saved successfully!");
+}
+
+function editProfile() {
+  var storedProfile = localStorage.getItem("profile");
+  if (storedProfile) {
+    var profile = JSON.parse(storedProfile);
+
+    console.log("Current Profile Information:");
+    console.log(profile);
+
+    var firstName = prompt(
+      "Enter your new first name (current: " + profile.FirstName + "):"
+    );
+    var lastName = prompt(
+      "Enter your new last name (current: " + profile.LastName + "):"
+    );
+    var username = prompt(
+      "Enter your new username (current: " + profile.Username + "):"
+    );
+    var email = prompt(
+      "Enter your new email address (current: " + profile.Email + "):"
+    );
+
+    profile.FirstName = firstName || profile.FirstName;
+    profile.LastName = lastName || profile.LastName;
+    profile.Username = username || profile.Username;
+    profile.Email = email || profile.Email;
+
+    var updatedProfileString = JSON.stringify(profile);
+    localStorage.setItem("profile", updatedProfileString);
+
+    console.log("Profile information updated successfully!");
+  } else {
+    console.log("No profile found. Please create a profile first.");
+  }
+}
+
+// Example usage:
+// Save profile
+saveProfile();
+
+// Edit profile
+editProfile();
+
+//Testing out new ways to save and edit profile in app
